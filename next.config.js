123605847bccd6510161cffbdf548dbc87f7a3b4
hsyl388 <husnsoyl388@gmail.com>
2025-04-06 16:31:36 +0300

    hata

--- next.config.js
+++ next.config.js
@@ -1,1 +1,9 @@
-module.exports = { reactStrictMode: true };
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    output: "standalone", // Vercel için doğru yapı
+  };
+  
+  module.exports = nextConfig;
+  
